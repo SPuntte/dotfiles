fcb3ed3c41d3870e94be14456bdd154edef32112
Pontus Lundström <pontus.lundstrom@gmail.com>
2019-09-16 11:55:23 +0300

    Auto-add more ssh keys

--- zsh/.oh-my-zsh/custom/ssh-agent.zsh
+++ zsh/.oh-my-zsh/custom/ssh-agent.zsh
@@ -9,7 +9,7 @@ function start_agent {
 	chmod 600 "${SSH_ENV}"
 	/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
 	. "${SSH_ENV}" > /dev/null
-	/usr/bin/ssh-add
+	/usr/bin/ssh-add $HOME/.ssh/id_*[^.pub]
 }
 
 # Source SSH settings, if applicable
