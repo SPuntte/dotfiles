16121c02e6d530b816ce3bfd16816abbad839270
Pontus Lundström <pontus.lundstrom@aalto.fi>
2019-12-09 16:05:49 +0200

    Store FQDN-specific SSH environment
    Hosts that share a home folder (e.g. via NFS) previously overwrote
    .ssh/environment at shell startup. This caused them to "leak" ssh-agent
    processes.
    
    This is now mitigated by storing a separate environment in a file with
    the FQDN of the host appended in its name and by copying it over the
    "global" environment at shell startup.

--- zsh/.oh-my-zsh/custom/ssh-agent.zsh
+++ zsh/.oh-my-zsh/custom/ssh-agent.zsh
@@ -2,18 +2,21 @@
 
 # Set up ssh-agent
 SSH_ENV="$HOME/.ssh/environment"
+SSH_LOCAL_ENV="${SSH_ENV}-$(hostname --fqdn)"
 
 function start_agent {
 	echo "Initializing new SSH agent..."
-	touch $SSH_ENV
-	chmod 600 "${SSH_ENV}"
-	/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+	touch $SSH_LOCAL_ENV
+	chmod 600 "${SSH_LOCAL_ENV}"
+	/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_LOCAL_ENV}"
+	cp -f "${SSH_LOCAL_ENV}" "${SSH_ENV}"
 	. "${SSH_ENV}" > /dev/null
 	/usr/bin/ssh-add $HOME/.ssh/id_*[^.pub]
 }
 
 # Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
+if [ -f "${SSH_LOCAL_ENV}" ]; then
+	cp -f "${SSH_LOCAL_ENV}" "${SSH_ENV}"
 	. "${SSH_ENV}" > /dev/null
 	kill -0 $SSH_AGENT_PID 2>/dev/null || {
 		start_agent
